c71d1adfa7c5f9fc78291687dfff04327d2f9830
sveinpg <sveinpg.gmail.com>
2019-10-20 14:22:06 +0200

    Use prettier

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,36 +3,36 @@ import { graphql } from 'gatsby';
 import { Helmet } from 'react-helmet';
 
 const Template = ({ data }) => {
-  const { markdownRemark } = data;
-  const { frontmatter, html } = markdownRemark;
+    const { markdownRemark } = data;
+    const { frontmatter, html } = markdownRemark;
 
-  return (
-    <main>
-      <Helmet>
-        <title>{frontmatter.title}</title>
-        <meta property="og:title" content={frontmatter.title} />
-        <meta property="og:description" content={frontmatter.ingress} />
-        <meta property="og:image" content={frontmatter.image} />
-      </Helmet>
-      <article>
-        <h1>{frontmatter.title}</h1>
-        <section dangerouslySetInnerHTML={{ __html: html }} />
-      </article>
-    </main>
-  );
+    return (
+        <main>
+            <Helmet>
+                <title>{frontmatter.title}</title>
+                <meta property="og:title" content={frontmatter.title} />
+                <meta property="og:description" content={frontmatter.ingress} />
+                <meta property="og:image" content={frontmatter.image} />
+            </Helmet>
+            <article>
+                <h1>{frontmatter.title}</h1>
+                <section dangerouslySetInnerHTML={{ __html: html }} />
+            </article>
+        </main>
+    );
 };
 
 export const aboutPageQuery = graphql`
-  query PostPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        title
-        ingress
-        image
-      }
+    query PostPage($id: String!) {
+        markdownRemark(id: { eq: $id }) {
+            html
+            frontmatter {
+                title
+                ingress
+                image
+            }
+        }
     }
-  }
 `;
 
 export default Template;
