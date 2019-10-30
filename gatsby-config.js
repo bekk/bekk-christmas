16f4623ae6d9fd5f873215783c9be61b72217524
sveinpg <sveinpg.gmail.com>
2019-10-30 11:10:26 +0100

    Fix typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         {
             resolve: `gatsby-source-filesystem`,
             options: {
-                path: `${__dirname}/post/opensources`,
+                path: `${__dirname}/post/opensource`,
             },
         },
         `gatsby-transformer-remark`,
