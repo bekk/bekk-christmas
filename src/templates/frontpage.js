284d7692fc073b9ecc7d9f1fcab8dba041a14127
Johanne Håøy Horn <johanne.hh@gmail.com>
2019-11-13 22:10:24 +0100

    Adjust front page to match design

--- src/templates/frontpage.js
+++ src/templates/frontpage.js
@@ -1,28 +1,68 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
+import styled from 'styled-components';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 import Calendar from '../components/Calendar';
 import CalendarWindowOpen from '../components/CalendarWindowOpen';
 
 import { getWindowImagePlaceholder } from '../utils';
+import Preview from './Preview';
 
-const Template = ({ pageContext }) => {
+const Header = styled.h1`
+    font-size: 2em;
+    font-weight: 400;
+`;
+
+const Description = styled.div`
+    margin-left: 50px;
+    margin-bottom: 150px;
+    max-width: 350px;
+    font-size: 1.4em;
+`;
+
+const DailyWindowHeader = styled.h2`
+    font-family: FFDINWebProLight, sans-serif;
+    font-weight: 400;
+    font-size: 1.8em;
+`;
+
+const Frontpage = ({ data, pageContext }) => {
+    const calendars = data.allMarkdownRemark.nodes
+        .filter(markdown => markdown.frontmatter.calendar != null)
+        .map(markdown => markdown.frontmatter);
+
+    const showTeaser = calendars.length === 0;
+
     return (
         <Layout>
-            {pageContext.calendarsWithContent && pageContext.calendarsWithContent.length > 0 && (
-                <Calendar>
-                    {pageContext.calendarsWithContent.map((link, index) => (
-                        <li key={link}>
-                            <CalendarWindowOpen
-                                link={link}
-                                imageUrl={getWindowImagePlaceholder(index)}
-                                title={link}
-                            />
-                        </li>
-                    ))}
-                </Calendar>
+            {showTeaser ? (
+                <div />
+            ) : (
+                <>
+                    <Description>
+                        <Header>Bekk Christmas</Header>
+                        <p>
+                            288 articles, 24 days.
+                            <br />
+                            Made with 🎅 in Oslo, Norway!{' '}
+                        </p>
+                    </Description>
+                    <DailyWindowHeader>Dagens luke</DailyWindowHeader>
+                    <Calendar>
+                        {calendars.map((calendar, index) => (
+                            <li key={calendar.calendar}>
+                                <CalendarWindowOpen
+                                    link={`${calendar.calendar}/${calendar.post_year}/${calendar.post_day}`}
+                                    imageUrl={getWindowImagePlaceholder(index)}
+                                    title={calendar.calendar}
+                                />
+                            </li>
+                        ))}
+                    </Calendar>
+                </>
             )}
+            <Preview calendarsWithContent={pageContext.calendarsWithContent} />
         </Layout>
     );
 };
@@ -44,4 +84,4 @@ export const frontpageQuery = graphql`
     }
 `;
 
-export default Template;
+export default Frontpage;
