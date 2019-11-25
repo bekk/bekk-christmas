f7904a78dfa93f1fc4effe4b40ec33add30ed781
Svein Petter Gjøby <sveinpg@users.noreply.github.com>
2019-11-25 15:48:29 +0100

    Update unsplash query parameters

--- src/utils.js
+++ src/utils.js
@@ -17,7 +17,7 @@ export const getWindowImagePlaceholder = index => {
 export const setImageWidth = url => {
     if (url.includes('unsplash')) {
         const urlPart = url.split('?')[0];
-        return urlPart + '?w=1000';
+        return urlPart + '?w=1226&h=400&fit=crop&crop=edges';
     }
 
     return url;
