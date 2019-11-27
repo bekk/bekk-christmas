750627949df6c52e8ce265fa0355fcb278eb5c9f
Dag Frode Solberg <dag.solberg@bekk.no>
2019-11-27 07:38:56 +0100

    Move image styling globally (#64)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,11 +9,20 @@ import GlobalStyles from './GlobalStyles';
 import Tre from './Tre';
 import { SkipToContent, MainContentWrapper } from './SkipToContent';
 
-const Container = styled.div`
+const Container = styled.div(
+    ({ theme }) => `
     display: flex;
     flex-direction: column;
     min-height: 100vh;
-`;
+
+    .dark-theme-image {
+        display: ${theme.darkThemeImageDisplay};
+    }
+    .light-theme-image {
+        display: ${theme.lightThemeImageDisplay};
+    }
+`
+);
 
 const MainContent = styled(MainContentWrapper)`
     flex: 1;
@@ -21,6 +30,13 @@ const MainContent = styled(MainContentWrapper)`
     max-width: 1600px;
     padding: 20px;
     margin: 0 auto;
+
+
+    .wide-image {
+        width: 100%;
+        height: 400px;
+        object-fit: cover;
+    }
 
     ${mediaQueries.smallUp}  {
         padding: 30px;
