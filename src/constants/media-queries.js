c77a511438906c9894b0b2a30210e0cb3cbf33e2
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2019-11-30 21:21:13 +0100

    Remove the flash when loading the page in dark mode

--- src/constants/media-queries.js
+++ src/constants/media-queries.js
@@ -1,3 +1,4 @@
 export const largeUp = '@media all and (min-width: 1008px)';
 export const mediumUp = '@media all and (min-width: 800px)';
 export const smallUp = '@media all and (min-width: 420px)';
+export const darkMode = '@media (prefers-color-scheme: dark)';
