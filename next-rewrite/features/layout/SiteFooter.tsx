4e46ab6c927601cc7d47786a3054567729c2d0ff
Anders Larsen <andelar96@gmail.com>
2021-08-09 12:23:15 +0200

    First go at making an RSS feed

--- next-rewrite/features/layout/SiteFooter.tsx
+++ next-rewrite/features/layout/SiteFooter.tsx
@@ -1,24 +1,16 @@
-import { Box, Button, Flex, Text, useColorMode } from "@chakra-ui/react";
-import * as React from "react";
-import { BekkLogo } from "../design-system/BekkLogo";
-import { TextLink } from "../design-system/TextLink";
+import { Box, Button, Flex, Text, useColorMode } from '@chakra-ui/react';
+import * as React from 'react';
+import { BekkLogo } from '../design-system/BekkLogo';
+import { TextLink } from '../design-system/TextLink';
 
 export const SiteFooter = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
-    <Box
-      as="footer"
-      maxWidth="80ch"
-      mx="auto"
-      mt={12}
-      mb={6}
-      textAlign="center"
-    >
+    <Box as="footer" maxWidth="80ch" mx="auto" mt={12} mb={6} textAlign="center">
       <BekkLogo maxWidth="150px" mx="auto" mb={6} />
       <Text fontSize="sm">
-        Bekk is all about craftmanship and the people crafting it. We have a
-        proud tradition of creating advent calendars, each with daily original
-        content made by us.
+        Bekk is all about craftmanship and the people crafting it. We have a proud tradition of
+        creating advent calendars, each with daily original content made by us.
       </Text>
       <Flex justifyContent="center">
         <TextLink mx={2} href="https://instagram.com/livetibekk">
@@ -36,15 +28,13 @@ export const SiteFooter = () => {
         <TextLink mx={2} href="https://blogg.bekk.no">
           Blog
         </TextLink>
+        <TextLink mx={2} href="https://bekk.christmas/rss.xml">
+          RSS
+        </TextLink>
       </Flex>
       <Box mt={6}>
-        <Button
-          size="sm"
-          variant="outline"
-          colorScheme="black"
-          onClick={toggleColorMode}
-        >
-          Turn {colorMode === "light" ? "off" : "on"} the lights!
+        <Button size="sm" variant="outline" colorScheme="black" onClick={toggleColorMode}>
+          Turn {colorMode === 'light' ? 'off' : 'on'} the lights!
         </Button>
       </Box>
     </Box>
