041818d819e197f1cfc0551fb2c765deab6fc15e
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-10-16 16:33:53 +0200

    Add common security headers

--- web/next.config.js
+++ web/next.config.js
@@ -6,4 +6,39 @@ module.exports = {
       "res.cloudinary.com",
     ],
   },
+  headers: async () => [
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "X-DNS-Prefetch-Control",
+          value: "on",
+        },
+        {
+          key: "Strict-Transport-Security",
+          value: "max-age=31536000; includeSubDomains; preload",
+        },
+        {
+          key: "X-XSS-Protection",
+          value: "1; mode=block",
+        },
+        {
+          key: "X-Frame-Options",
+          value: "SAMEORIGIN",
+        },
+        {
+          key: "Permissions-Policy",
+          value: "camera=(), microphone=(), geolocation=()",
+        },
+        {
+          key: "X-Content-Type-Options",
+          value: "nosniff",
+        },
+        {
+          key: "Referrer-Policy",
+          value: "origin-when-cross-origin",
+        },
+      ],
+    },
+  ],
 };
