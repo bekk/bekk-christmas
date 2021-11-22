bdec3eb9c3022a87a552e05d8e1182b327a61593
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-11-22 21:35:50 +0100

    Add support for NIX and Python code highlighting

--- web/features/portable-text/serializers/CodeBlock.tsx
+++ web/features/portable-text/serializers/CodeBlock.tsx
@@ -13,35 +13,39 @@ import jsx from "react-syntax-highlighter/dist/cjs/languages/prism/jsx";
 import kotlin from "react-syntax-highlighter/dist/cjs/languages/prism/kotlin";
 import less from "react-syntax-highlighter/dist/cjs/languages/prism/less";
 import html from "react-syntax-highlighter/dist/cjs/languages/prism/markup";
+import nix from "react-syntax-highlighter/dist/cjs/languages/prism/nix";
+import python from "react-syntax-highlighter/dist/cjs/languages/prism/python";
 import rust from "react-syntax-highlighter/dist/cjs/languages/prism/rust";
 import scala from "react-syntax-highlighter/dist/cjs/languages/prism/scala";
-import swift from "react-syntax-highlighter/dist/cjs/languages/prism/swift"
 import scss from "react-syntax-highlighter/dist/cjs/languages/prism/scss";
+import swift from "react-syntax-highlighter/dist/cjs/languages/prism/swift";
 import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
 import prism from "react-syntax-highlighter/dist/cjs/styles/prism/prism";
 
-SyntaxHighlighter.registerLanguage("html", html);
+SyntaxHighlighter.registerLanguage("bash", bash);
+SyntaxHighlighter.registerLanguage("clojure", clojure);
 SyntaxHighlighter.registerLanguage("css", css);
-SyntaxHighlighter.registerLanguage("scss", scss);
-SyntaxHighlighter.registerLanguage("less", less);
-SyntaxHighlighter.registerLanguage("js", jsx);
+SyntaxHighlighter.registerLanguage("elm", elm);
+SyntaxHighlighter.registerLanguage("fsharp", fsharp);
+SyntaxHighlighter.registerLanguage("haskell", haskell);
+SyntaxHighlighter.registerLanguage("html", html);
+SyntaxHighlighter.registerLanguage("java", java);
 SyntaxHighlighter.registerLanguage("javascript", jsx);
-SyntaxHighlighter.registerLanguage("jsx", jsx);
+SyntaxHighlighter.registerLanguage("js", jsx);
 SyntaxHighlighter.registerLanguage("json", json);
+SyntaxHighlighter.registerLanguage("jsx", jsx);
+SyntaxHighlighter.registerLanguage("kotlin", kotlin);
+SyntaxHighlighter.registerLanguage("less", less);
+SyntaxHighlighter.registerLanguage("nix", nix);
+SyntaxHighlighter.registerLanguage("python", python);
+SyntaxHighlighter.registerLanguage("rust", rust);
+SyntaxHighlighter.registerLanguage("scala", scala);
+SyntaxHighlighter.registerLanguage("scss", scss);
+SyntaxHighlighter.registerLanguage("sh", bash);
+SyntaxHighlighter.registerLanguage("swift", swift);
 SyntaxHighlighter.registerLanguage("ts", tsx);
 SyntaxHighlighter.registerLanguage("typescript", tsx);
 SyntaxHighlighter.registerLanguage("tsx", tsx);
-SyntaxHighlighter.registerLanguage("elm", elm);
-SyntaxHighlighter.registerLanguage("bash", bash);
-SyntaxHighlighter.registerLanguage("sh", bash);
-SyntaxHighlighter.registerLanguage("kotlin", kotlin);
-SyntaxHighlighter.registerLanguage("java", java);
-SyntaxHighlighter.registerLanguage("haskell", haskell);
-SyntaxHighlighter.registerLanguage("scala", scala);
-SyntaxHighlighter.registerLanguage("clojure", clojure);
-SyntaxHighlighter.registerLanguage("fsharp", fsharp);
-SyntaxHighlighter.registerLanguage("rust", rust);
-SyntaxHighlighter.registerLanguage("swift", swift);
 
 export const CodeBlock = ({ node }: any) => {
   if (!node?.code) {
