08a0dbd4b2ed56a331e798b65b0c65c221336be7
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-11-23 07:46:58 +0100

    Use Chakra UI's useInterval instead of implementing one ourselves

--- web/features/stand/useSlideshow.tsx
+++ web/features/stand/useSlideshow.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import tp from "timeproxy";
+import { useInterval } from "@chakra-ui/react";
 import { LogoSlide } from "./views/LogoSlide";
-import tp from 'timeproxy'
 
-const DEFAULT_DURATION = tp`ten seconds`;
+const DEFAULT_DURATION = tp`two seconds`;
 
 /** Show your slides with this useful little bugger
  *
@@ -28,6 +29,7 @@ export const useSlideshow = (
     () => slidesWithLogo[currentSlideIndex],
     [currentSlideIndex, slidesWithLogo]
   );
+
   useInterval(() => {
     const nextIndex = (currentSlideIndex + 1) % slidesWithLogo.length;
     setCurrentSlideIndex(nextIndex);
@@ -43,23 +45,3 @@ export const useSlideshow = (
 
   return Array.isArray(slideToShow) ? slideToShow[0] : slideToShow;
 };
-
-function useInterval(callback: Function, delay: number) {
-  const savedCallback = React.useRef<Function>();
-
-  // Remember the latest callback.
-  React.useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-
-  // Set up the interval.
-  React.useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
-}
