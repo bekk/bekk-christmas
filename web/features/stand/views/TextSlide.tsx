dc348aa3c5c26cadfbfc9f56bec75cb39cab67bf
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-11-23 08:41:24 +0100

    Tweak the look and add flexibility to the text slide

--- web/features/stand/views/TextSlide.tsx
+++ web/features/stand/views/TextSlide.tsx
@@ -1,13 +1,11 @@
-import { Box, Center } from "@chakra-ui/react";
+import { Box, BoxProps, Center } from "@chakra-ui/react";
 import React from "react";
 
-type TextSlideProps = { children: React.ReactNode };
-export const TextSlide = ({ children }: TextSlideProps) => {
+type TextSlideProps = { children: React.ReactNode } & BoxProps;
+export const TextSlide = (props: TextSlideProps) => {
   return (
     <Center minHeight="100vh" background="brand.pink" color="brand.darkGreen">
-      <Box px="2em" fontSize="5xl">
-        {children}
-      </Box>
+      <Box px="2em" fontSize="5xl" lineHeight="1.3" {...props} />
     </Center>
   );
 };
