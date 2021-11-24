37ea06fedd79ab6c50d6d167d456a6816aadff1b
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-11-24 21:22:55 +0100

    Add bekk-a-likes luke

--- web/pages/stand/index.tsx
+++ web/pages/stand/index.tsx
@@ -4,6 +4,7 @@ import tp from "timeproxy";
 import { useLuke } from "../../features/stand/useLuke";
 import { useSecondsLeft } from "../../features/stand/useSecondsLeft";
 import { useSlideshow } from "../../features/stand/useSlideshow";
+import { BekkalikeSlide } from "../../features/stand/views/BekkalikeSlide";
 import { CountdownSlide } from "../../features/stand/views/CountdownSlide";
 import { GenerativeArtSlide } from "../../features/stand/views/GenerativeArtSlide";
 import { ImageSlide } from "../../features/stand/views/ImageSlide";
@@ -461,7 +462,43 @@ https://priceless-bose-d509cc.netlify.app"
     ]);
   },
   Luke21: () => {
-    return useSlideshow([<TitleSlide key={1}>Bekk-a-likes</TitleSlide>]);
+    return useSlideshow([
+      <TitleSlide key={1}>Bekk-a-likes</TitleSlide>,
+      <BekkalikeSlide key={2} title="Kieran Culkin || August Dahl">
+        <Image src="/images/bekk-a-likes/01-both.png" alt="" />
+      </BekkalikeSlide>,
+      <BekkalikeSlide key={3} title="Andrew Garfield || Bendik Edvardsen">
+        <Image src="/images/bekk-a-likes/02-both.png" alt="" />
+      </BekkalikeSlide>,
+      <BekkalikeSlide key={4} title="Adam Conover || Didrik Sæther">
+        <Image src="/images/bekk-a-likes/03-celeb.png" alt="" />
+        <Image src="/images/bekk-a-likes/03-bekker.png" alt="" />
+      </BekkalikeSlide>,
+      <BekkalikeSlide key={5} title="Seeb || Erik Wendel">
+        <Image src="/images/bekk-a-likes/04-both.png" alt="" />
+      </BekkalikeSlide>,
+      <BekkalikeSlide
+        key={6}
+        title="Bølle-til-bestevenn-Marie || Vikki Walle-Hansen"
+      >
+        <Image src="/images/bekk-a-likes/05-celeb.png" alt="" />
+        <Image src="/images/bekk-a-likes/05-bekker.png" alt="" />
+      </BekkalikeSlide>,
+      <BekkalikeSlide key={7} title="Rufus Sewell || Håvard Hvassing">
+        <Image src="/images/bekk-a-likes/06-both.png" alt="" />
+      </BekkalikeSlide>,
+      <BekkalikeSlide key={8} title="Pewdiepie || Jøran Vagnby Lillesand">
+        <Image src="/images/bekk-a-likes/07-celeb.png" alt="" />
+        <Image src="/images/bekk-a-likes/07-bekker.png" alt="" />
+      </BekkalikeSlide>,
+      <BekkalikeSlide key={9} title="Andreas Wahl || Andreas Heim">
+        <Image src="/images/bekk-a-likes/08-celeb.png" alt="" />
+        <Image src="/images/bekk-a-likes/08-bekker.png" alt="" />
+      </BekkalikeSlide>,
+      <BekkalikeSlide key={10} title="Ryan Gosling || Herman Slyngstadli">
+        <Image src="/images/bekk-a-likes/09-both.png" alt="" />
+      </BekkalikeSlide>,
+    ]);
   },
   Luke22: () => {
     return useSlideshow([<TitleSlide key={1}>Dagens artikler</TitleSlide>]);
