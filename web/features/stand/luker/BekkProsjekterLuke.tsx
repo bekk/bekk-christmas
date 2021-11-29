aa0680a2e6522d1d070681cd109f7b6ae7f028ef
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-11-29 22:44:15 +0100

    Tweak the design of the Bekk projects luke

--- web/features/stand/luker/BekkProsjekterLuke.tsx
+++ web/features/stand/luker/BekkProsjekterLuke.tsx
@@ -6,23 +6,35 @@ import { TitleSlide } from "../views/TitleSlide";
 
 export const BekkProsjekterLuke = () => {
   return useSlideshow([
-    <TitleSlide key={1}>Bekk-prosjekter</TitleSlide>,
+    <TitleSlide key={0}>Bekk-prosjekter</TitleSlide>,
+    <TextSlide key={1}>
+      <Text>
+        Bekk driver med mye rart. Her er et par prosjekter – både interne og
+        eksterne – vi er ekstra stolte av 🤩
+      </Text>
+    </TextSlide>,
     <TextSlide key={2}>
-      <Heading>{"<ForrigeUke />"}</Heading>
+      <Heading fontSize="7xl" fontWeight="normal">
+        {"<ForrigeUke />"}
+      </Heading>
       <Text>
         {"<ForrigeUke />"} er en artikkelserie på blogg.bekk.no, som oppsummerer
         hva som skjedde i frontend-verden i uken som var
       </Text>
     </TextSlide>,
     <TextSlide key={3}>
-      <Heading>Drypp</Heading>
+      <Heading fontSize="7xl" fontWeight="normal">
+        Drypp
+      </Heading>
       <Text>
         Drypp er en lavterskel podcast der vi snakker om innovasjon,
         produktutvikling, forretningsutvikling og ledelse.
       </Text>
     </TextSlide>,
     <TextSlide key={4}>
-      <Heading>Den Norske Opera og Ballett</Heading>
+      <Heading fontSize="7xl" fontWeight="normal">
+        Den Norske Opera og Ballett
+      </Heading>
       <Text>Vi fikk mulighet til å revitalisere Operaen.no</Text>
       <Text>
         For å bedre gjenspeile det moderne uttrykket til bygget og den åpne
@@ -31,19 +43,19 @@ export const BekkProsjekterLuke = () => {
       </Text>
     </TextSlide>,
     <TextSlide key={5}>
-      <Heading>Pils og Programmering</Heading>
+      <Heading fontSize="7xl" fontWeight="normal">
+        Pils og Programmering
+      </Heading>
       <Text>
         «Pils og programmering» er et enkelt konsept: en samling for folk som
         liker å ta et par pils mens de progger i godt lag. Ta med deg maskinen
         og en idé du bryr deg om, og slipp skaperevnen løs blant likesinnede.
       </Text>
-      <Text>
-        Og selvfølgelig, du trenger ikke drikke pils. Du får også alkoholfritt
-        og brus. Og mat. 🍕
-      </Text>
     </TextSlide>,
     <TextSlide key={6}>
-      <Heading>Klimabrølet</Heading>
+      <Heading fontSize="7xl" fontWeight="normal">
+        Klimabrølet
+      </Heading>
       <Text>
         Klimabrølet jobber for å bygge en folkelig oppslutning for å påvirke
         politikere til å ta klimaet på alvor. Teamet har jobbet med løsningen
@@ -52,7 +64,9 @@ export const BekkProsjekterLuke = () => {
       </Text>
     </TextSlide>,
     <TextSlide key={7}>
-      <Heading>Entur Tavla</Heading>
+      <Heading fontSize="7xl" fontWeight="normal">
+        Entur Tavla
+      </Heading>
       <Text>
         Entur Tavla lar deg enkelt lage din egen avgangstavle for den adressen
         du ønsker i hele Norge, med tilpasset visningsformat, antall
@@ -60,7 +74,9 @@ export const BekkProsjekterLuke = () => {
       </Text>
     </TextSlide>,
     <TextSlide key={8}>
-      <Heading>Pushwagnesizer</Heading>
+      <Heading fontSize="7xl" fontWeight="normal">
+        Pushwagnesizer
+      </Heading>
       <Text>
         I samarbeid med Grafill, fikk Bekk være med å konseptualisere temaet for
         ED Awards, «Making new connections». Målet var å skape en unik
