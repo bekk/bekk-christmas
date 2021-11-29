20d655d598031e75cda381413da9b714b2881081
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-11-30 00:37:57 +0100

    Add intro text to fagdagsminner

--- web/features/stand/luker/FagdagsminnerLuke.tsx
+++ web/features/stand/luker/FagdagsminnerLuke.tsx
@@ -1,15 +1,23 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 import tp from "timeproxy";
 import { useSlideshow } from "../useSlideshow";
+import { TextSlide } from "../views/TextSlide";
 import { TitleSlide } from "../views/TitleSlide";
 
 export const FagdagsminnerLuke = () => {
   return useSlideshow([
     <TitleSlide key={1}>Fagdags-minner</TitleSlide>,
+    <TextSlide key={2}>
+      <Text>
+        Tre ganger i året tar alle Bekkere seg fri fra prosjekt for å lære nye
+        ting, henge med kollegaer og ha det moro.
+      </Text>
+      <Text>Her er noen klipp fra hvordan det ser ut!</Text>
+    </TextSlide>,
     [
       <Box
-        key={2}
+        key={3}
         as="iframe"
         src="https://player.vimeo.com/video/651274092?h=953ea434b5&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1&amp;loop=1"
         width="100vw"
