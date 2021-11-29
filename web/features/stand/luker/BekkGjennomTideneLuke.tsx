c131a49de185f774f8108bd4704d2ae6235adfe1
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-11-30 00:41:08 +0100

    Add some info to the bekk.no gjennom tidene luke

--- web/features/stand/luker/BekkGjennomTideneLuke.tsx
+++ web/features/stand/luker/BekkGjennomTideneLuke.tsx
@@ -1,11 +1,23 @@
+import { Text } from "@chakra-ui/react";
 import React from "react";
 import { useSlideshow } from "../useSlideshow";
 import { FullScreenImageSlide } from "../views/FullScreenImageSlide";
+import { TextSlide } from "../views/TextSlide";
 import { TitleSlide } from "../views/TitleSlide";
 
 export const BekkGjennomTideneLuke = () => {
   return useSlideshow([
     <TitleSlide key={1}>Bekk.no gjennom tidene</TitleSlide>,
+    <TextSlide key={1.5}>
+      <Text>
+        Selv om vi har det mye moro, er Bekk et ganske stabilt firma, med solide
+        nettsider som varer noen år.
+      </Text>
+      <Text>
+        Nå har vi gravd dypt i arkivet for å se hvordan vi så ut på nettet fra
+        starten av!
+      </Text>
+    </TextSlide>,
     <FullScreenImageSlide
       key={2}
       src="/images/bekkno-gjennom-tidene/bekkno-2000.png"
