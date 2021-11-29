bd6979ee879dab8d2d1f3a839f2178f2d0ca45de
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-11-29 19:59:54 +0100

    Add slides for bekk.no gjennom tidene

--- web/features/stand/luker/BekkGjennomTideneLuke.tsx
+++ web/features/stand/luker/BekkGjennomTideneLuke.tsx
@@ -1,14 +1,50 @@
+import { Image } from "@chakra-ui/react";
 import React from "react";
 import { useSlideshow } from "../useSlideshow";
-import { TextSlide } from "../views/TextSlide";
 import { TitleSlide } from "../views/TitleSlide";
 
 export const BekkGjennomTideneLuke = () => {
   return useSlideshow([
-    <TitleSlide key={1}>Bekk gjennom tidene</TitleSlide>,
-    <TextSlide key={2}>
-      <strong>Bekk er 21 år i år.</strong> Her er en liten historie om hvordan
-      vi har blitt de vi er.
-    </TextSlide>,
+    <TitleSlide key={1}>Bekk.no gjennom tidene</TitleSlide>,
+    <Image
+      key={2}
+      width="100vw"
+      height="100vh"
+      objectFit="cover"
+      src="/images/bekkno-gjennom-tidene/bekkno-2000.png"
+      alt="Bekk.no i 2000"
+    />,
+    <Image
+      key={3}
+      width="100vw"
+      height="100vh"
+      objectFit="cover"
+      src="/images/bekkno-gjennom-tidene/bekkno-2002.png"
+      alt="Bekk.no i 2002"
+    />,
+    <Image
+      key={4}
+      width="100vw"
+      height="100vh"
+      objectFit="cover"
+      src="/images/bekkno-gjennom-tidene/bekkno-2007.png"
+      alt="Bekk.no i 2007"
+    />,
+    <Image
+      key={5}
+      width="100vw"
+      height="100vh"
+      objectFit="cover"
+      src="/images/bekkno-gjennom-tidene/bekkno-2012.png"
+      alt="Bekk.no i 2012"
+    />,
+    <Image
+      key={6}
+      width="100vw"
+      height="100vh"
+      objectFit="cover"
+      src="/images/bekkno-gjennom-tidene/bekkno-2021.png"
+      alt="Bekk.no i 2021"
+    />,
   ]);
 };
