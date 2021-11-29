5cbed3949cf7026d25baf1df9089c4331bacf682
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-11-30 00:42:08 +0100

    Add critical emoji

--- web/features/stand/luker/TastekonkurranseLuke.tsx
+++ web/features/stand/luker/TastekonkurranseLuke.tsx
@@ -18,7 +18,7 @@ export const TastekonkurranseLuke = () => {
 https://priceless-bose-d509cc.netlify.app"
       />
       <Text fontSize="3xl" textAlign="center">
-        (vi har en data stående her også)
+        (Vi har en data stående her også 👇)
       </Text>
     </TextSlide>,
   ]);
