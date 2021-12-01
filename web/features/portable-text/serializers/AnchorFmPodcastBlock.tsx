ec431053636ab52ed503b80fc540712e760ef43e
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-12-01 10:14:25 +0100

    tweak podcasts more

--- web/features/portable-text/serializers/AnchorFmPodcastBlock.tsx
+++ web/features/portable-text/serializers/AnchorFmPodcastBlock.tsx
@@ -3,5 +3,5 @@ import React from "react";
 
 type AnchorFmPodcastBlockProps = { node: { src: string } };
 export const AnchorFmPodcastBlock = ({ node }: AnchorFmPodcastBlockProps) => {
-  return <Box as="iframe" src={node.src} frameBorder="0" scrolling="no" />;
+  return <Box as="iframe" src={node.src} frameBorder="0" scrolling="no" width="100%" />;
 };
