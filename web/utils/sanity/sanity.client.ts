792de18ac16756bdad4de7ab13ad5c1c6a3f850e
Anders Larsen <andelar96@gmail.com>
2022-10-05 15:55:54 +0200

    Fjern ubrukt kode

--- web/utils/sanity/sanity.client.ts
+++ web/utils/sanity/sanity.client.ts
@@ -1,12 +1,6 @@
-import {
-  createCurrentUserHook,
-  createPreviewSubscriptionHook,
-} from "next-sanity";
+import { createPreviewSubscriptionHook } from "next-sanity";
 import { sanityConfig } from "./config";
 
 /** Use the usePreviewSubscription hook to get preview details */
 export const usePreviewSubscription =
   createPreviewSubscriptionHook(sanityConfig);
-
-/** Gets the logged in Sanity user (if any) */
-export const useCurrentUser = createCurrentUserHook(sanityConfig);
