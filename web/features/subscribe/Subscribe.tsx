08cd1b3127da5eac457b4eb2e7233b036849d661
Anders Larsen <andelar96@gmail.com>
2022-11-04 09:41:37 +0100

    Prøver å legge inn mailerlite form i stedet

--- web/features/subscribe/Subscribe.tsx
+++ web/features/subscribe/Subscribe.tsx
@@ -12,8 +12,6 @@ import {
 import React from "react";
 
 const Subscribe = () => {
-  const [value, setValue] = React.useState("1");
-
   return (
     <Stack
       bg="white"
@@ -22,35 +20,7 @@ const Subscribe = () => {
       borderRadius="1rem"
       border="2px solid black"
     >
-      <Heading fontWeight="normal" color="brand.darkGreen">
-        Join in the holiday cheer and count down to christmas with us!
-      </Heading>
-      <Text as="b">I would like updates:</Text>
-      <RadioGroup onChange={setValue} value={value}>
-        <Stack>
-          <Radio value="1" colorScheme="green">
-            For each calendar window
-          </Radio>
-          <Radio value="2" colorScheme="green">
-            Weekly summaries
-          </Radio>
-        </Stack>
-      </RadioGroup>
-      <Flex>
-        <Input
-          placeholder="Type your email..."
-          colorScheme="green"
-          borderTopRightRadius={0}
-          borderBottomRightRadius={0}
-        />
-        <Button
-          colorScheme="green"
-          borderTopLeftRadius={0}
-          borderBottomLeftRadius={0}
-        >
-          Subscribe
-        </Button>
-      </Flex>
+      <div className="ml-embedded" data-form="qxYVax" />
     </Stack>
   );
 };
