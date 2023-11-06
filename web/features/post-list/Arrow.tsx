80176c39e0958bd9e007044fbe65bdc94e927879
Anders Larsen <andelar96@gmail.com>
2023-11-06 20:30:52 +0100

    Legger til en pil for å utløse en deploy elns

--- web/features/post-list/Arrow.tsx
+++ web/features/post-list/Arrow.tsx
@@ -1,6 +1,15 @@
 import { Box, BoxProps } from "@chakra-ui/react";
 import React from "react";
 
+export const Arrow = (props: BoxProps) => (
+  <Box as="svg" viewBox="0 0 20 20" fill="none" {...props}>
+    <path
+      d="M11.0425 15.3845L12.1621 16.437L18.5811 9.99564L12.1543 3.59119L11.036 4.66473L15.7832 9.21066L1.01269 9.20167L1.01367 10.813L15.7842 10.822L11.0425 15.3845Z"
+      fill="currentColor"
+    />
+  </Box>
+);
+
 export const ArrowShort = (props: BoxProps) => {
   return (
     <Box
